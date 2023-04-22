1f6c8724f085380520a824616e3becd4496baf0a
Robin Altman <robinhaltman@gmail.com>
2023-04-21 22:38:42 -0400

    WORKING JAVASCRIPT

--- script.js
+++ script.js
@@ -21,15 +21,6 @@ navigator.mediaDevices.getUserMedia(audioIN)
   let mediaRecorder = new MediaRecorder(mediaStreamObj);
   let isRecording = false;
 
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("demo");
-  output.innerHTML = slider.value; // Display the default slider value
-
-  // Update the current slider value (each time you drag the slider handle)
-  slider.oninput = function() {
-    output.innerHTML = this.value;
-  }
-
   recordButton.addEventListener('click', function (ev) {
     if (isRecording) {
       //stop recording
