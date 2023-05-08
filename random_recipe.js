cd79306d862276172b8631a02bcdc446557b9f98
Robin Altman <robinhaltman@gmail.com>
2023-05-07 21:17:00 -0400

    Finishing touches pt 1

--- random_recipe.js
+++ random_recipe.js
@@ -10,12 +10,29 @@
       })
       .catch(error => console.error(error));
 
+      var selectedLines = '';
+      for (var i = 0; i < 5; i++) {
+        var randomIndex = Math.floor(Math.random() * lines.length);
+        selectedLines += lines[randomIndex] + '<br><br>';
+      }
+      randText.innerHTML = selectedLines;
+
     gen.onclick = function() {
       var selectedLines = '';
       for (var i = 0; i < 5; i++) {
         var randomIndex = Math.floor(Math.random() * lines.length);
-        selectedLines += lines[randomIndex] + '<br>';
+        selectedLines += lines[randomIndex] + '<br><br>';
       }
       randText.innerHTML = selectedLines;
     };
   });
+
+ //read about statment aloud
+
+ // Get reference to the div element
+var myDiv = document.getElementById("about");
+var myAudio = document.getElementById("myAudio");
+
+myDiv.addEventListener("click", function() {
+  myAudio.play();
+});
